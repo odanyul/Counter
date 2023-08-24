80577b2d3865a89b577a09c92cfd05690dc7b407
Adam Szekely <adamsz@tuta.io>
2023-08-24 18:20:16 +0200

    Format with prettier

--- src/Image.jsx
+++ src/Image.jsx
@@ -5,7 +5,7 @@ function Image() {
     return <img src="http://placekitten.com/200/300" alt="cicus" />;
 }
 
-export const CUTEST_CAT = 'mine';
-export const CUTEST_DOG = 'also mine';
+export const CUTEST_CAT = "mine";
+export const CUTEST_DOG = "also mine";
 
 export default Image;

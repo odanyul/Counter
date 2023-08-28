71cf4c190353c0e10a6c6b9b66a606174817deae
Adam Szekely <adamsz@tuta.io>
2023-08-28 18:39:14 +0200

    [08.28] Text visibility

--- src/08.28/Counter.jsx
+++ src/08.28/Counter.jsx
@@ -9,6 +9,7 @@ function Counter() {
 
     const [count, setCount] = useState(0);
     const [text, setText] = useState("bla");
+    const [textVisible, setTextVisible] = useState(true);
 
     const handleClick = () => {
         const nextCount = count + 1;
@@ -16,12 +17,30 @@ function Counter() {
         setText(text + "bla");
     };
 
+    const toggleTextVisibility = () => {
+        if (textVisible) {
+            setTextVisible(false);
+        } else {
+            setTextVisible(true);
+        }
+    };
+
     return (
         <>
             <h1>Counter</h1>
             <h3>{count}</h3>
-            <p>{text}</p>
+            {/* <p
+                style={{
+                    display: textVisible ? "block" : "none",
+                }}
+            >
+                {text}
+            </p> */}
+            {textVisible && <p>{text}</p>}
             <button onClick={handleClick}>+1</button>
+            <button onClick={toggleTextVisibility}>
+                {textVisible ? "Hide text" : "Show text"}
+            </button>
         </>
     );
 }
